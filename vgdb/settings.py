fdad387b78f25130414ef5b69c3f2ca1691080e9
Arg Galinato <rrargh@yahoo.com>
2016-09-27 21:58:29 +0800

    Updated settings file

--- vgdb/settings.py
+++ vgdb/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'vgdb.urls'
@@ -139,6 +140,7 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
